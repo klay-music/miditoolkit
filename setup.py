b0a29bc714f8648de39316e488bcdb910a3ec301
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2023-05-05 11:52:01 +0200

    add matplotlib dep

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='miditoolkit',
-    version='0.1.16',
-    description='',
-    author='wayne391',
-    author_email='s101062219@gmail.com',
-    url='https://github.com/YatingMusic/miditoolkit',
+    name="miditoolkit",
+    version="0.1.16",
+    description="",
+    author="wayne391",
+    author_email="s101062219@gmail.com",
+    url="https://github.com/YatingMusic/miditoolkit",
     packages=find_packages(),
-    package_data={'': ['examples_data/*']},
+    package_data={"": ["examples_data/*"]},
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Topic :: Multimedia :: Sound/Audio :: MIDI",
     ],
-    keywords='music midi mir',
-
-    license='MIT',
+    keywords="music midi mir",
+    license="MIT",
     install_requires=[
-        'numpy >= 1.7.0',
-        'mido >= 1.1.16',
-    ]
+        "numpy >= 1.7.0",
+        "mido >= 1.1.16",
+        "matplotlib",
+    ],
 )
 
 
